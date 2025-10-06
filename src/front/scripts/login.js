d04595eda144562ca3fecf7ac2d0f3b6bbcd476d
Nalanda Luiza dos Santos <103153823+NalandaLiu@users.noreply.github.com>
2025-10-05 22:20:30 -0300

    login.js

--- src/front/scripts/login.js
+++ src/front/scripts/login.js
@@ -27,11 +27,11 @@ botaoLogar.addEventListener("click", async ()=>{
 
             const token = data.token;
     
-            // Set the JWT as a cookie (valid for 7 days, secure, and same-site restricted)
+        
             document.cookie = `token=${token}; path=/; max-age=${7 * 24 * 60 * 60}; SameSite=Strict`;
     
             console.log('JWT armazenado nos cookies');
-            // Redirect to profile page
+        
             window.location.href = "http://127.0.0.1:5501/src/front/pages/perfil.html";
         } else {
             console.error("O login falhou: Status - " + response.status);
@@ -39,7 +39,6 @@ botaoLogar.addEventListener("click", async ()=>{
             errorMessage.textContent = 'O usuário ou a senha estão incorretos';
         }
     } catch (error) {
-        // Log any error that occurs during the fetch or processing
         console.error("Erro durante a solicitação de login:", error);
     }
 });
